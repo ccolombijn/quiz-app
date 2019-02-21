7df112fcb80e14af2fe6cd199afa22f34d67ec48
ccolombijn <ccolombijn@gmail.com>
2019-02-21 11:34:57 +0100

    Added first draft application framework files

--- class/Quiz.js
+++ class/Quiz.js
@@ -2,7 +2,10 @@ class Quiz {
   constructor( id, name, questions ) {
     this._id = id
     this._name = name
-    this._questions = questions 
+    this._questions = questions
+
+  }
+  start(){
 
   }
 }
