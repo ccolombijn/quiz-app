a01ff913955b9b6406b37f4b7509987f4471dd62
ccolombijn <ccolombijn@gmail.com>
2019-03-08 10:41:00 +0100

    Added get, post, put & delete for questions, anwsers & game

--- api/app.js
+++ api/app.js
@@ -22,18 +22,21 @@ const api = (function(){
       user: 'root',
       password: 'root',
       database: 'quiz'
-    },
-    routes : [
-      'game','game/:id','questions','questions/:id','anwsers','anwsers/:id' ]
+    }//,routes : [ 'game','game/:id','questions','questions/:id','anwsers','anwsers/:id' ]
   }
   const connection = mysql.createConnection( config.db )
-
-
-  /* -----------------------------------------------------------------------------
-  * api.get
+  connection.connect((err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Connected!');
+    }
+  });
+// .............................................................................
+// questions
+/* -----------------------------------------------------------------------------
+    * questions get
   */
-  // api.get( { table : 'tables'[, key : 'id'] } )
-
   app.get( `/api/questions/:id`, function( req, res ) {
 
     let id = +req.params[ 'id' ]
@@ -62,59 +65,170 @@ const api = (function(){
     })
   });
 
-
-  const get = function( args, callback ){
-    const table = args.table,
-    key = args.key
-
-    if( key ){
-      app.get( `/api/${table}/:${key}`, function( req, res ) {
-
-        let pointer = +req.params[ key ]
-        // bad request
-        if( pointer.indexOf( '=' ) ){
-          res.setHeader('Content-Type', 'application/json')
-          res.status(400).end()
-          return
-        }
-        connection.query(`SELECT * FROM ${table} where ${key}=?`, pointer, ( err, rows ) => {
-          if (!err) {
-            let record = rows[0];
-            res.setHeader('Content-Type', 'application/json')
-            record ? res.end(JSON.stringify( record ) ) : res.status(404).end()
-          } else {
-            throw err
-          }
-        })
-      })
-    } else {
-      app.get(`/api/${table}`, function(req, res) {
-
-        res.setHeader('Content-Type', 'application/json')
-
-        connection.query(`SELECT * FROM ${table}`, ( err, records ) => {
-          if (!err) {
-            res.end( JSON.stringify(records) )
-          } else {
-            throw err
-          }
-        })
-      })
-    }
-    if( callback ) callback()
-  }
-  /* -----------------------------------------------------------------------------
-  * api.put
+/* -----------------------------------------------------------------------------
+  * questions post
   */
 
-  const put = function( args, callback ){
-    const table = args.table,
-    fields = args.fields,
-    key = args.key;
+  app.post('/api/questions', function(req, res) {
 
-    app.put( `/api/${table}/:${key}`, function( req, res ) {
+  let question = req.body;
+  connection.query('INSERT INTO questions SET ?', question, (err, result) => {
+    if (!err) {
+      res.setHeader('Content-Type', 'application/json')
+      connection.query('SELECT * FROM questions where id=?', result.insertId, (err, rows) => {
+        if (!err) {
+          let question = rows[0];
+          if (question) {
+            res.setHeader('Content-Type', 'application/json')
+            res.status(201).end(JSON.stringify(question));
+          } else {
+            res.setHeader('Content-Type', 'application/json')
+            res.status(404).end();
+          }
+        } else {
+          throw err;
+        }
+      });
+    } else {
+      throw err;
+    }
+  });
+});
 
-      let pointer = +req.params[ key ]
+/* -----------------------------------------------------------------------------
+  * questions put
+  */
+
+  app.put( `/api/questions/:id`, function( req, res ) {
+
+    let id = +req.params[ 'id' ]
+    // bad request
+    if( pointer.indexOf( '=' ) ){
+      res.setHeader('Content-Type', 'application/json')
+      res.status(400).end()
+      return
+    }
+
+    let body = req.body
+    let query = `UPDATE ${table} SET `
+    let query_fields = []
+    for( let field in fields ){
+      query += `${fields[field]} = ?`
+      if( field < fields.length ) query += ','
+      query_fields.push( body[ fields[ field ] ])
+    }
+    query += `WHERE ${key} = ?`
+    query_fields.push( pointer )
+    connection.query( query, query_fields, ( err, result ) => {
+      if (!err) {
+        connection.query(`SELECT * FROM ${table} where ${key}=?`, [ pointer ], ( err, rows ) => {
+          if (!err) {
+            let table = rows[0]
+            if (table) {
+              res.setHeader('Content-Type', 'application/json')
+              res.end(JSON.stringify(table))
+            } else {
+              res.setHeader('Content-Type', 'application/json')
+              res.status(404).end()
+            }
+          } else {
+            throw err
+          }
+        })
+      }else {
+        throw err
+      }
+    })
+  });
+
+
+
+/* -----------------------------------------------------------------------------
+  * questions delete
+  */
+
+  app.delete(`/api/questions/:id`, function( req, res ) {
+    let id =+req.params['id']
+
+    connection.query( `DELETE FROM questions WHERE id = ?`, [id], ( err, result ) => {
+      if (!err) {
+        res.status(204).end();
+      } else {
+          throw err;
+      }
+    })
+  })
+
+// .............................................................................
+// anwsers
+/* -----------------------------------------------------------------------------
+  * anwsers get
+  */
+    app.get( `/api/anwsers/:id`, function( req, res ) {
+
+      let id = +req.params[ 'id' ]
+
+      connection.query(`SELECT * FROM anwsers where id=?`, id, ( err, rows ) => {
+        if (!err) {
+          let record = rows[0];
+          res.setHeader('Content-Type', 'application/json')
+          record ? res.end(JSON.stringify( record ) ) : res.status(404).end()
+        } else {
+          throw err
+        }
+      })
+    });
+
+    app.get(`/api/anwsers`, function(req, res) {
+
+      res.setHeader('Content-Type', 'application/json')
+
+      connection.query(`SELECT * FROM anwsers`, ( err, records ) => {
+        if (!err) {
+          res.end( JSON.stringify(records) )
+        } else {
+          throw err
+        }
+      })
+    });
+
+/* -----------------------------------------------------------------------------
+  * anwsers post
+  */
+
+    app.post('/api/anwsers', function(req, res) {
+
+    let anwser = req.body;
+    connection.query('INSERT INTO anwsers SET ?', anwser, (err, result) => {
+      if (!err) {
+        res.setHeader('Content-Type', 'application/json')
+        connection.query('SELECT * FROM anwsers where id=?', result.insertId, (err, rows) => {
+          if (!err) {
+            let anwser = rows[0];
+            if (anwser) {
+              res.setHeader('Content-Type', 'application/json')
+              res.status(201).end(JSON.stringify(anwser));
+            } else {
+              res.setHeader('Content-Type', 'application/json')
+              res.status(404).end();
+            }
+          } else {
+            throw err;
+          }
+        });
+      } else {
+        throw err;
+      }
+    });
+  });
+
+  /* -----------------------------------------------------------------------------
+    * anwsers put
+  */
+
+    app.put( `/api/anwsers/:id`, function( req, res ) {
+
+      let id = +req.params[ 'id' ]
       // bad request
       if( pointer.indexOf( '=' ) ){
         res.setHeader('Content-Type', 'application/json')
@@ -152,21 +266,18 @@ const api = (function(){
           throw err
         }
       })
-    })
-    if( callback ) callback()
-  }
+    });
+
+
 
   /* -----------------------------------------------------------------------------
-  * api.drop
+    * anwsers delete
   */
-  const drop = function( args, callback ){
-    const table = args.table,
-    fields = args.fields,
-    key = args.key;
-    app.delete(`/api/${table}/:${key}`, function( req, res ) {
-      let pointer = +req.params[ key ]
 
-      connection.query( `DELETE FROM ${table} WHERE ${key} = ?`, [id], ( err, result ) => {
+    app.delete(`/api/anwsers/:id`, function( req, res ) {
+      let id =+req.params['id']
+
+      connection.query( `DELETE FROM anwsers WHERE id = ?`, [id], ( err, result ) => {
         if (!err) {
           res.status(204).end();
         } else {
@@ -174,8 +285,146 @@ const api = (function(){
         }
       })
     })
-    if( callback ) callback()
-  }
+
+
+
+
+    // .............................................................................
+    // game
+    /* -----------------------------------------------------------------------------
+      * game get
+      */
+        app.get( `/api/game/:id`, function( req, res ) {
+
+          let id = +req.params[ 'id' ]
+
+          connection.query(`SELECT * FROM game where id=?`, id, ( err, rows ) => {
+            if (!err) {
+              let record = rows[0];
+              res.setHeader('Content-Type', 'application/json')
+              record ? res.end(JSON.stringify( record ) ) : res.status(404).end()
+            } else {
+              throw err
+            }
+          })
+        });
+
+        app.get(`/api/game`, function(req, res) {
+
+          res.setHeader('Content-Type', 'application/json')
+
+          connection.query(`SELECT * FROM game`, ( err, records ) => {
+            if (!err) {
+              res.end( JSON.stringify(records) )
+            } else {
+              throw err
+            }
+          })
+        });
+
+    /* -----------------------------------------------------------------------------
+      * game post
+      */
+
+        app.post('/api/game', function(req, res) {
+
+        let anwser = req.body;
+        connection.query('INSERT INTO game SET ?', anwser, (err, result) => {
+          if (!err) {
+            res.setHeader('Content-Type', 'application/json')
+            connection.query('SELECT * FROM game where id=?', result.insertId, (err, rows) => {
+              if (!err) {
+                let item = rows[0];
+                if (item) {
+                  res.setHeader('Content-Type', 'application/json')
+                  res.status(201).end(JSON.stringify(item));
+                } else {
+                  res.setHeader('Content-Type', 'application/json')
+                  res.status(404).end();
+                }
+              } else {
+                throw err;
+              }
+            });
+          } else {
+            throw err;
+          }
+        });
+      });
+
+      /* -----------------------------------------------------------------------------
+        * game put
+      */
+
+        app.put( `/api/game/:id`, function( req, res ) {
+
+          let id = +req.params[ 'id' ]
+          // bad request
+          if( pointer.indexOf( '=' ) ){
+            res.setHeader('Content-Type', 'application/json')
+            res.status(400).end()
+            return
+          }
+
+          let body = req.body
+          let query = `UPDATE ${table} SET `
+          let query_fields = []
+          for( let field in fields ){
+            query += `${fields[field]} = ?`
+            if( field < fields.length ) query += ','
+            query_fields.push( body[ fields[ field ] ])
+          }
+          query += `WHERE ${key} = ?`
+          query_fields.push( pointer )
+          connection.query( query, query_fields, ( err, result ) => {
+            if (!err) {
+              connection.query(`SELECT * FROM ${table} where ${key}=?`, [ pointer ], ( err, rows ) => {
+                if (!err) {
+                  let table = rows[0]
+                  if (table) {
+                    res.setHeader('Content-Type', 'application/json')
+                    res.end(JSON.stringify(table))
+                  } else {
+                    res.setHeader('Content-Type', 'application/json')
+                    res.status(404).end()
+                  }
+                } else {
+                  throw err
+                }
+              })
+            }else {
+              throw err
+            }
+          })
+        });
+
+
+
+      /* -----------------------------------------------------------------------------
+        * game delete
+      */
+
+        app.delete(`/api/game/:id`, function( req, res ) {
+          let id =+req.params['id']
+
+          connection.query( `DELETE FROM game WHERE id = ?`, [id], ( err, result ) => {
+            if (!err) {
+              res.status(204).end();
+            } else {
+                throw err;
+            }
+          })
+        })
+
+
+
+
+
+
+
+
+
+
 
   const server = app.listen(8081, () => {
 
